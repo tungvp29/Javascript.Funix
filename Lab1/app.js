8e180ec3ff127c0f5a5b748db763d11c7d4a4c64
tungvp29 <37463451+tungvp29@users.noreply.github.com>
2022-12-05 00:11:14 +0700

    Lab1 + Lab 2

---
+++ Lab1/app.js
@@ -0,0 +1,15 @@
+//Lab 1:
+let country = 'VietNam';
+let continent = 'Asia';
+let population = 100;
+console.log(`Country: ${country}, Continent: ${continent}, Population: ${population}`);
+
+//Lab 2.1 - 2.5:
+let isIsland = false;
+let language;
+console.log(`isIsland: ${isIsland}, Population: ${population}, Country: ${country}, Language: ${language}`);
+
+//Lab 2.2:
+language = 'Vietnamese';
+const name = 'J';
+
